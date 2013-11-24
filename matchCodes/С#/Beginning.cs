09b97749f5d0c28f14037c2ec4538bbb61df26ed
pavel.khrapkin@gmail.com <pavel.khrapkin@gmail.com@c288bfc6-c2a2-388e-821e-69711807850a>
2013-11-24 09:17:00 +0000

    Beginning.cs - начало работы приложения match. Запуск происходит либо по событию Open File в Excel, либо при активном запуске ActiveStart.

---
+++ matchCodes/С#/Beginning.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Box = System.Windows.Forms.MessageBox;
+using Lib = match.MatchLib;
+
+namespace ExcelAddIn2
+{
+    class Beginning
+    {
+        public void test(Excel.Workbook Wb) {
+            Document contracts = new Document("Платежи");
+            System.Windows.Forms.MessageBox.Show("Opening WB='"+Wb.Name+"' лист[1]='"+
+                Wb.Sheets[1].Name+"'  строк="+ Lib.EOL(Wb.Sheets[1]));
+//            Box.Show("Opening WB='"+Wb.Name+"' лист(Договоры)='"+
+  //              Wb.Sheets["Договоры"].Name + "'  строк=" + Lib.EOL(Wb.Sheets["Договоры"]));
+        }
+        public void ActiveStart() {
+            Box.Show("ActiveStart");
+//            Excel.Application app = new Excel.Application();
+//            Document contracts = new Document("Платежи");
+        }
+
+    }
+}
