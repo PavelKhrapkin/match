fd6a3754930d298bc4cbfa0456882327168a74fd
pavel.khrapkin@gmail.com <pavel.khrapkin@gmail.com@c288bfc6-c2a2-388e-821e-69711807850a>
2013-12-01 22:16:08 +0000

    Добавлен List<int> ToIntList

--- matchCodes/С#/MatchLib.cs
+++ matchCodes/С#/MatchLib.cs
@@ -1,11 +1,13 @@
 ﻿/*-----------------------------------------------------------------------
  * MatchLib -- библиотека общих подпрограмм проекта match 3.0
  * 
- *  20.11.2013  П.Храпкин, А.Пасс
+ *  1.12.2013  П.Храпкин, А.Пасс
  *  
  * - 20.11.13 переписано с VBA на С#
+ * - 1.12.13 добавлен метод ToIntList
  * -------------------------------------------
- * EOL(Sh)          - возвращает число непустых строк в листе Sh
+ * EOL(Sh)                  - возвращает число непустых строк в листе Sh
+ * ToIntList(s, separator)  - возвращает List<int>, разбирая строку s с разделителями separator
  */
 using System;
 using System.Collections.Generic;
@@ -15,7 +17,7 @@ using Excel = Microsoft.Office.Interop.Excel;
 
 namespace match
 {
-    class MatchLib {
+    public static class MatchLib {
         public static int EOL(Excel.Worksheet Sh)
         /*
          * возвращает число непустых строк листа Sh 
@@ -24,9 +26,6 @@ namespace match
          */
         {
             int i;
-//            int j = Sh.UsedRange.Rows.Count;
-//            System.Windows.Forms.MessageBox.Show("Sh.Name='"+Sh.Name+"'  Count="+j);
-//            for (i = j; i > 0; i--) {
             for (i = Sh.UsedRange.Rows.Count; i > 0; i--) {
                 for (int col = 1; col <= Sh.UsedRange.Columns.Count; col++)
                 {
@@ -38,6 +37,20 @@ namespace match
                 };
             }
             return i;
+        }
+        /*
+         *  преобразует строку, разделенную символами separator, в List<int>.
+         *  не преобразуемые компоненты пропускаются.
+         */
+        public static List<int> ToIntList(string s, char separator)
+        {
+            string[] ar = s.Split(separator);
+            List<int> ints = new List<int>();
+            foreach (var item in ar) {
+                int v;
+                if (int.TryParse(item, out v)) ints.Add(v);
+            }
+            return ints;
         }
         
     }
