0ffd5b845c2b72f1fc1a3e9e12a32594bd1f7d53
pavel.khrapkin@gmail.com <pavel.khrapkin@gmail.com@c288bfc6-c2a2-388e-821e-69711807850a>
2013-12-08 23:03:24 +0000

    отлажена загрузка коллекции Documents из TOCmatch. Теперь дописывается recognizeDoc(wb) и getDoc(name). В частности, проверка Штампа в checkStamp(rng,stampList)

--- matchCodes/С#/Document.cs
+++ matchCodes/С#/Document.cs
@@ -1,15 +1,20 @@
 ﻿/*-----------------------------------------------------------------------
  * Document -- класс Документов проекта match 3.0
  * 
- *  8.12.2013  П.Храпкин, А.Пасс
+ *  9.12.2013  П.Храпкин, А.Пасс
  *  
- * - 8.12.13 переписано с VBA TOCmatch на С#
+ * - 9.12.13 переписано с VBA TOCmatch на С#
  * -------------------------------------------
- * Document(name)       - КОНСТРУКТОР возвращает ОБЪЕКТ Документ с именем name, при необходимости - открывает его
- * getDoc(name)         - ????
+ * Document(name)       - КОНСТРУКТОР возвращает ОБЪЕКТ Документ с именем name
  * loadDoc(name, wb)    - загружает Документ name или его обновления из файла wb
+ * getDoc(name)         - возвращает Документ с именем name; при необходимости - открывает его
  * isDocOpen(name)      - проверяет, что Документ name открыт
  * recognizeDoc(wb)     - распознает первый лист файла wb по таблице Штампов
+ * checkStamp(rng,stampList)       - проверка Штампов stampList в Range rng
+ * 
+ * внутренний класс Stamp предназначен для заполнения списков Штампов
+ * каждый Штамп содержит сигнатуру, то есть проверяемый текст, и пар координат - его положений
+ * Stamp(Range rng)     - разбирает rng, помещая из таблицы TOCmatch Штамп в List Штампов в Документе
  */
 using System;
 using Box = System.Windows.Forms.MessageBox;
@@ -35,6 +40,7 @@ namespace ExcelAddIn2
         private int EOLinTOC;
         private List<Stamp> stampList;
         public Excel.Range Body;
+        public Excel.Range Summary;
 
         public const string F_MATCH = "match.xlsm";
         private const string TOC = "TOCmatch";
@@ -76,7 +82,12 @@ namespace ExcelAddIn2
                     Documents.Add(docName,doc);
                     doc.stampList = new List<Stamp>();  //каждый документ ссыылается на цепочку сигнатор-Штамп
                 }
-                doc.stampList.Add(new Stamp(rw.Range["J1:M1"]));
+                Excel.Range rwStamp = rw.Range["J1:M1"];
+                if (rwStamp.Cells[1, 2].value.ToString()[0] != 'N') // тип Штампа 'N' - проверка Штампа не нужна
+                {
+                    doc.stampList.Add(new Stamp(rwStamp));
+                }
+
             }
             if (dirDBs != (string)db_match.Worksheets[TOC].cells[1, TOC_DIRDBS_COL].Value2)
             {
@@ -98,13 +109,45 @@ namespace ExcelAddIn2
             return Documents[name];
         }
 
+        public Document getDoc(string name)
+        {
+            try
+            {
+                Document doc = Documents[name];
+                if (!doc.isOpen)
+                {
+                    // загрузка Документа из файла
+                }
+                Excel.Range rng = (doc.FileName == F_SFDC) ? doc.Summary : doc.Body;
+//!!!!!                if (!checkStamp(rng, doc.stampList))
+                {
+                    // фатальная ошибка
+                }
+                return doc;
+            }
+            catch
+            {
+                return null;
+            }
+    // ДОПИСАТЬ!! Но вначале надо отладить recognizeDoc. При этом скорее всего внутреннюю часть цикла
+    // проверки Штампов для Документа надо будет перенести в метод checkStamp
+        }
+
+        private bool checkStamp(Excel.Range rng, List<Stamp> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool checkStamp(Excel.Range rng, Document stampList)
+        {
+            return false;
+        }
+ 
         public bool isDocOpen(string name) { return (Documents.ContainsKey(name)); }
 
         public static string recognizeDoc(Excel.Workbook wb) {
             Excel.Worksheet wholeSheet = wb.Worksheets[1];
             Excel.Range rng = wholeSheet.Range["1:" + Lib.EOL(wholeSheet)];
-
-
 
             foreach (var doc in Documents)
             {
@@ -123,7 +166,6 @@ namespace ExcelAddIn2
         private static Excel.Workbook fileOpen(string name) {
             Excel.Application app = new Excel.Application();
             app.Visible = true;
-            //            Microsoft.Office.Interop.Excel.Workbook wb;
             foreach (Excel.Workbook W in app.Workbooks)
             {
                 if (W.Name == name)
@@ -144,32 +186,27 @@ namespace ExcelAddIn2
         }
 
         protected class Stamp {
-            public string signature; // проверяемый текст Штампа - сигнатура
+            public string signature;  // проверяемый текст Штампа - сигнатура
             private char typeStamp;   // '=' - точное соответствие сигнатуры; 'I' - "текст включает.."
-
-            // альтернативная позиция Штампа, если есть, сохраняется во второй компоненте массива
-            public List<int[]> stampPosition = new List<int[]>();      // позиция сигнатуры в проверяемом Документе
+            public List<int[]> stampPosition = new List<int[]>();   // альтернативные позиции сигнатур Штампов
 
             public Stamp(Excel.Range rng)
             {
-                signature = rng.Cells[1, 1].value;                          // {[1, "1, 6"]} --> [1,1] или [1,6]
-                string str = rng.Cells[1, 2].value;
+                signature = rng.Cells[1, 1].value;  // примеры: {[1, "1, 6"]} --> [1,1] или [1,6]
+                string str = rng.Cells[1, 2].value; // .. {["4,1", "2,3"]} --> [4,2]/[4,3]/[1,2]/[1,3]
                 typeStamp = str[0];
+
                 List<int> rw = Lib.ToIntList(rng.Cells[1, 3].value.ToString(), ',');
                 List<int> col = Lib.ToIntList(rng.Cells[1, 4].value.ToString(), ',');
 
-                // привести массивы rw и col к наибольшей длине  
-                for (int i = col.Count; i < rw.Count; i++) col.Add(rw[i]);
-                for (int i = rw.Count; i < col.Count; i++) rw.Add(col[i]);
-
-                // перенести результат в коллекцию пар stampPosition
-                for (int i = 0; i < col.Count; i++) {
-                    int[] x = {rw[i],col[i]};
-                    stampPosition.Add(x);        
+                for (int i = 0; i < rw.Count; i++)
+                {
+                    for (int j = 0; j < col.Count; j++)
+                    {
+                        int[] x = { rw[i], col[j] };
+                        stampPosition.Add(x);
+                    }
                 }
-           }
-            public bool checkStamp(Excel.Workbook wb, Stamp stamp) {
-                return false;
             }
         }    // конец класса Stamp      
     }    // конец класса Document
