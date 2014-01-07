1cef028b456eee40d77a8f30640b23ace5f8fa80
pavel.khrapkin@gmail.com <pavel.khrapkin@gmail.com@c288bfc6-c2a2-388e-821e-69711807850a>
2014-01-07 16:42:49 +0000

    добавлена секция констант Шаблонов

--- matchCodes/С#/match 3.0.1/Declaration.cs
+++ matchCodes/С#/match 3.0.1/Declaration.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace match.Declaration
 {
+    /// <summary>
+    /// Declaration -- определения глобальных констант
+    /// </summary>
+    /// <journal> 20.12.2013
+    /// 7.1.2014    - добавлена секция констант Шаблонов
+    /// </journal>
     class Declaration
     {
         /// <summary>
@@ -33,35 +39,43 @@ namespace match.Declaration
         /// </summary>
         public const string F_TMP = "W_TMP.xlsm";
 
-        public const string dirDBs = "C:\\Users\\Pavel_Khrapkin\\Documents\\Pavel\\match\\matchDBs\\";
+        public const string DIR_DBS = "ENV_DIR_DBS";
+
         //--------- листы match.xlsm --------------
-        public const string WE      = "We";
+        public const string WE = "We";
         public const string HEADER = "Header";
 
         //-----------константы таблицы Документов -----------------
-        public const string DOC_TIME            = "A1"; // дата и время последнего изменения Документа
-        public const string DOC_NAME            = "B1"; // имя Документа
-        public const string DOC_EOL             = "C1"; // EOL Документа
-        public const string DOC_MYCOL           = "D1"; // количество колонок, дабавляемых слева в Документ в loadDoc
-        public const string DOC_RESLINES        = "E1"; // число строк пятки
-        public const string DOC_MADESTEP        = "F1"; // последний выполненный Шаг
-        public const string DOC_FILE            = "H1"; // файл match, содержащий Документ
-        public const string DOC_SHEET           = "I1"; // лист Документа
-        public const string DOC_PATTERN         = "P1"; // основной шаблон Документа
+        public const string DOC_TIME        = "A1"; // дата и время последнего изменения Документа
+        public const string DOC_NAME        = "B1"; // имя Документа
+        public const string DOC_EOL         = "C1"; // EOL Документа
+        public const string DOC_MYCOL       = "D1"; // количество колонок, дабавляемых слева в Документ в loadDoc
+        public const string DOC_RESLINES    = "E1"; // число строк пятки
+        public const string DOC_MADESTEP    = "F1"; // последний выполненный Шаг
+        public const string DOC_FILE        = "H1"; // файл match, содержащий Документ
+        public const string DOC_SHEET       = "I1"; // лист Документа
+        public const string DOC_CREATED     = "N1"; // дата создания Документа
+        public const string DOC_PATTERN     = "P1"; // основной шаблон Документа
         public const string DOC_SUMMARY_PATTERN = "Q1"; // шаблон пятки
-        public const string DOC_LOADER          = "T1"; // Loader Документа
+        public const string DOC_LOADER      = "T1"; // Loader Документа
 
         //-----------константы таблицы Процессов -----------------
-        public const string STEP_TIME =     "A1";
-        public const string STEP_NAME =     "B1";
-        public const string STEP_PREV =     "C1";
-        public const string STEP_COMMENT =  "D1";
-        public const string STEP_DONE =     "E1";
-        public const string STEP_PARAM =    "F1";
-        public const string STEP_PARAMS =   "F1:J1";
-        public const string STEP_DOCS =     "K1:O1";
+        public const string STEP_TIME   = "A1";
+        public const string STEP_NAME   = "B1";
+        public const string STEP_PREV   = "C1";
+        public const string STEP_COMMENT = "D1";
+        public const string STEP_DONE   = "E1";
+        public const string STEP_PARAM  = "F1";
+        public const string STEP_PARAMS = "F1:J1";
+        public const string STEP_DOCS   = "K1:O1";
 
         public const string PROC_NAME = STEP_PARAM;
         public const string PROC_DOCUMENTS = "K1:O1";
+
+        //-----------константы Шаблонов -----------------
+        public const string PTRN_HDR    = "A1";     // заголовки колонок   
+        public const string PTRN_WIDTH  = "A3";     // ширина колонок
+
+        public const string PTRN_COPYHDR = "CopyHdr"; // указание копировать заголовок из Шаблона
     }
 }
