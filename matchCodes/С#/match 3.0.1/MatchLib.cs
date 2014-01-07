a4295748cf23d2121bdb6d4387d167371d7ba5f4
pavel.khrapkin@gmail.com <pavel.khrapkin@gmail.com@c288bfc6-c2a2-388e-821e-69711807850a>
2014-01-07 16:52:54 +0000

    - быстрый EOL (A.Pass) - тестирование DateTime и константы веремени компилляции (#if)

--- matchCodes/С#/match 3.0.1/MatchLib.cs
+++ matchCodes/С#/match 3.0.1/MatchLib.cs
@@ -1,14 +1,22 @@
 ﻿/*-----------------------------------------------------------------------
  * MatchLib -- библиотека общих подпрограмм проекта match 3.0
  * 
- *  28.12.2013  П.Храпкин, А.Пасс
+ *  07.01.14 П.Храпкин, А.Пасс
  *  
  * - 20.11.13 переписано с VBA на С#
  * - 1.12.13 добавлен метод ToIntList
+ * - 1.1.14 добавлен ToStrList с перегрузками и умолчаниями
+ * - 02.01.14 двоичный поиск EOL
+ *            добавлен метод getDateTime(dynamic inp)
  * -------------------------------------------
  * EOL(Sh)                  - возвращает число непустых строк в листе Sh
  * ToIntList(s, separator)  - возвращает List<int>, разбирая строку s с разделителями separator
+ * ToStrList(s,[separator]) - возвращает List<string> из Range или из строки s с разделителем
  */
+
+#define binarySearchEOL
+//#define TimeTest
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -80,6 +88,75 @@ namespace match.Lib
             return false;
         }
         //    */
+#if binarySearchEOL
+        /// <summary>
+        ///  EOL(Worksheet Sh)   - возвращает число непустых строк листа Sh
+        /// </summary>
+        /// <param name="Sh"></param>
+        /// <returns></returns>
+        /// <journal>21.11.2013
+        /// 13.12.13 - ограничение количества просматриваемых колонок до 20
+        /// 25.12.13 - модификация для усечения файлов с очень длинным пустым хвостом
+        /// 28.12.13 - bug fix - при обнаружении непустой ячейки break в цикле по строке
+        /// 02.01.14 - применен алгоритм двоичного поиска пустой строки
+        ///         с поиском MAX_EMPTY_ROWS пустых строк в конце
+        /// </journal>
+        const int MAX_COL = 20;         // максимальное число просматриваемых колонок листа
+        const int MAX_EMPTY_ROWS = 10;  // максимальное допустимое число пустых строк не в конце листа, 
+        public static int EOL(Excel.Worksheet Sh)
+        {
+            int rowCount = Sh.UsedRange.Rows.Count;
+            int colCount = Sh.UsedRange.Columns.Count;
+            if (rowCount <= 0) return 0;        // особый случай
+
+            int beg = 1, end = rowCount + 1;    // beg всегда показывает на непустую строку, end - на пустую или последнюю 
+            int indx = (beg + end) / 2;
+            while ((end - beg) > 1)
+            {
+                if (indx < rowCount && isLineFilled(Sh, indx, colCount))
+                {
+                    beg = indx;
+                    indx += (end - indx) / 2;
+                }
+                else
+                {
+                    end = indx;
+                    indx -= (indx - beg) / 2;
+                }
+            }
+            //
+            // проверяем, что за найденной строкой есть MAX_EMPTY_ROWS пустых (или конец листа)
+            // найдя непустую строку - запоминаем ее как найденную и сдвигаемся
+            //
+            //            bool notEmpty = true;
+            for (end = beg + MAX_EMPTY_ROWS; indx < end; indx++)
+            {
+                if (indx > rowCount)
+                {
+                    return beg;
+                }
+                if (isLineFilled(Sh, indx, colCount))
+                {
+                    beg = indx;
+                    end = beg + MAX_EMPTY_ROWS;
+                }
+            }
+            return beg;
+        }   // end EOL()
+        /*
+         * возвращает true, если в строке есть непустые ячейки
+         */
+        //        private static bool isLineFilled(Excel.Worksheet sh, int row)
+        private static bool isLineFilled(Excel.Worksheet sh, int row, int cols)
+        {
+            cols = Math.Min(cols, MAX_COL);
+            for (int col = 1; col <= cols; col++)
+            {
+                if (!isCellEmpty(sh, row, col)) return true;
+            }
+            return false;
+        }
+#else
         /// <summary>
         ///  EOL(Worksheet Sh)   - возвращает число непустых строк листа Sh
         /// </summary>
@@ -122,12 +199,13 @@ namespace match.Lib
             }
             return 0;
         }
+#endif
         /// <summary>
         /// ToStrList(Excel.Range)  - возвращает лист строк, содержащийся в ячеках
         /// </summary>
         /// <param name="rng"></param>
         /// <returns>List<streeng></streeng></returns>
-        /// <jornal> 31.12.2013 P.Khrapkin
+        /// <jornal> 1.1.2014 P.Khrapkin
         /// </jornal>
         public static List<string> ToStrList(Excel.Range rng)
         {
@@ -148,7 +226,22 @@ namespace match.Lib
             foreach (var item in ar) strs.Add(item);
             return strs;
         }
-
+        /// <summary>
+        /// если в rng null, пусто или ошибка - возвращаем 0, а иначе целое число
+        /// </summary>
+        /// <param name="rng"></param>
+        /// <returns></returns>
+        public static int RngToInt(Excel.Range rng)
+        {
+            int v = 0;
+            try
+            {
+                string str = rng.Text;
+                int.TryParse(str, out v);
+            }
+            catch { v = 0; }
+            return v;
+        }
         /// <summary>
         /// ToIntList(s, separator) - разбирает строку s с разделительным символом separatop;
         ///                           возвращает List int найденных целых чисел
@@ -183,11 +276,96 @@ namespace match.Lib
             var value = sh.UsedRange.Cells[row, col].Value2;
             return (value == null || value.ToString().Trim() == "");
         }
-    }
+
+#if TimeTest
+        public static int TestTime()
+        {
+            DateTime _t = getDateTime(null);
+            Console.WriteLine("{0} -- отладка getDateTime(null)", _t);
+
+            _t = getDateTime(DateTime.Now);
+            Console.WriteLine("{0} -- отладка getDateTime(Now)", _t);
+
+            _t = getDateTime("1/1/2014 0:0:0 PM");
+            Console.WriteLine("{0} -- отладка getDateTime(1/1/2014 0:0:0 PM)", _t);
+
+            _t = getDateTime("1/12/2013 0:0:0 PM");
+            Console.WriteLine("{0} -- отладка getDateTime(1/12/2013 0:0:0 PM)", _t);
+
+            _t = getDateTime("31/12/2013 0:0:0 PM");
+            Console.WriteLine("{0} -- отладка getDateTime(31/12/2013 0:0:0 PM)", _t);
+
+            _t = Lib.MatchLib.getDateTime("12/31/2013 0:0:0 PM");
+            Console.WriteLine("{0} -- отладка getDateTime(12/31/2013 0:0:0 PM)", _t);
+
+            _t = DateTime.Now;
+            Console.WriteLine("{0} -- отладка getDateTime(Now)", _t.ToShortDateString());
+            return 0;
+        }
+#endif
+        /// <summary>
+        /// getDateTime(dynamic inp)     - возвращает DateTime для любого значения (из ячейки Excel)
+        /// </summary>
+        /// <param name="inp"></param>
+        /// <returns>DateTime</returns>
+        /// <journal> 02.01.14 A.Pass
+        /// </journal>
+        public static DateTime getDateTime(dynamic inp)
+        {
+
+            if (inp == null) return new DateTime(0);
+            if (inp.GetType() == typeof(string))
+            {
+                DateTime ret;
+                try
+                {
+                    Double x = Double.Parse(inp);
+                    ret = DateTime.FromOADate(x);
+                    return ret;
+                }
+                catch
+                {
+                    string[] pDate = inp.Split(' ');
+                    string[] day = pDate[0].Split('/');
+                    string[] time = pDate[1].Split(':');
+                    int hour = Convert.ToInt32(time[0]);    //час
+                    if (pDate[2] == "PM" && hour != 12) hour += 12; // 12 PM - полдень
+                    else if (hour == 12) hour = 0;          // 12 АМ - полночь
+                    try
+                    {
+                        return new DateTime(
+                             Convert.ToInt32(day[2])        //год
+                            , Convert.ToInt32(day[1])       //мес
+                            , Convert.ToInt32(day[0])       //день
+                            , hour
+                            , Convert.ToInt32(time[1])      //мин
+                            , Convert.ToInt32(time[2]));    //сек
+                    }
+                    catch
+                    {
+                        return new DateTime(
+                             Convert.ToInt32(day[2])        //год
+                            , Convert.ToInt32(day[0])       //мес
+                            , Convert.ToInt32(day[1])       //день
+                            , hour
+                            , Convert.ToInt32(time[1])      //мин
+                            , Convert.ToInt32(time[2]));    //сек
+                    }
+                }
+            }
+            else if (inp.GetType() == typeof(Double))
+            {
+                return DateTime.FromOADate(inp);
+            }
+            return inp;
+        }
+    }   // конец класса MatchLib
 
     /// <summary>
     /// Log & Dump System
     /// </summary>
+    /// <journal> 30.12.2013 P.Khrapkin
+    /// </journal>
     public class Log
     {
         private static string _context;
@@ -196,16 +374,20 @@ namespace match.Lib
         public Log(string msg)
         {
             _context = "";
-            foreach (string name in _nameStack)  _context = name + ">" + _context;
-            Console.WriteLine(DateTime.Now.TimeOfDay + " " + _context + " " + msg);
+            foreach (string name in _nameStack) _context = name + ">" + _context;
+            Console.WriteLine(DateTime.Now.ToLongTimeString() + " " + _context + " " + msg);
         }
-        public static void set(string sub)  { _nameStack.Push(sub); }
-        public static void exit()           { _nameStack.Pop(); }
+        public static void set(string sub) { _nameStack.Push(sub); }
+        public static void exit() { _nameStack.Pop(); }
         public static void FATAL(string msg)
         {
             new Log("[FATAL] " + msg);
             System.Diagnostics.Debugger.Break();
         }
         public static void Warning(string msg) { new Log("[warning] " + msg); }
+        public static void START(string msg)
+        {
+            Console.WriteLine(DateTime.Now.ToShortDateString() + " ---------< " + msg + " >---------");
+        }
     }
 }
